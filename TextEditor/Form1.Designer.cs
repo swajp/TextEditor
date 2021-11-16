8adbaa4035c27e3935f82eafbcecdb8137bfc5f3
Miroslav Stejskal <Stejskal.Miroslav@stud.vassboskovice.cz>
2021-11-16 13:23:41 +0100

    -

--- TextEditor/Form1.Designer.cs
+++ TextEditor/Form1.Designer.cs
@@ -28,13 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button buttonOpen;
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSaveAs = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textContent = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            buttonOpen = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            buttonOpen.Location = new System.Drawing.Point(13, 0);
+            buttonOpen.Name = "buttonOpen";
+            buttonOpen.Size = new System.Drawing.Size(75, 28);
+            buttonOpen.TabIndex = 0;
+            buttonOpen.Text = "Otevřít";
+            buttonOpen.UseVisualStyleBackColor = true;
+            buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonSaveAs);
+            this.panel1.Controls.Add(this.buttonSave);
+            this.panel1.Controls.Add(this.labelName);
+            this.panel1.Controls.Add(buttonOpen);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(801, 28);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonSaveAs
+            // 
+            this.buttonSaveAs.Enabled = false;
+            this.buttonSaveAs.Location = new System.Drawing.Point(175, 0);
+            this.buttonSaveAs.Name = "buttonSaveAs";
+            this.buttonSaveAs.Size = new System.Drawing.Size(75, 28);
+            this.buttonSaveAs.TabIndex = 0;
+            this.buttonSaveAs.Text = "Uložit jako";
+            this.buttonSaveAs.UseVisualStyleBackColor = true;
+            this.buttonSaveAs.Click += new System.EventHandler(this.buttonSaveAs_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(94, 0);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 28);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "Uložit";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelName.Location = new System.Drawing.Point(544, 7);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(12, 15);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "-";
+            // 
+            // textContent
+            // 
+            this.textContent.Location = new System.Drawing.Point(-1, 27);
+            this.textContent.Multiline = true;
+            this.textContent.Name = "textContent";
+            this.textContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textContent.Size = new System.Drawing.Size(801, 431);
+            this.textContent.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textContent);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textContent;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Button buttonSaveAs;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- TextEditor/Form1.cs
+++ TextEditor/Form1.cs
@@ -7,15 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TextEditor
 {
     public partial class Form1 : Form
     {
+        string filePath = "";
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "All files|*.*|C# class|*.cs*|TXT files|*.txt*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                labelName.Text = openFileDialog1.SafeFileName;
+                if (File.Exists(filePath))
+                {
+                    textContent.Text = File.ReadAllText(filePath);
+                    buttonSave.Enabled = true;
+                    buttonSaveAs.Enabled = true;
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(filePath, textContent.Text);
+            MessageBox.Show("Soubor uložen");
+        }
+
+        private void buttonSaveAs_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "NewFile.txt";
+            saveFileDialog1.Filter = "Text file|*.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = saveFileDialog1.FileName;
+                Text = filePath;
+                File.WriteAllText(filePath, textContent.Text);
+            }
+        }
     }
 }
